284510b266541f9635403756bb1ee386b2b3c98b
fzuellich <fzuellich@gmail.com>
2012-03-15 20:44:12 +0100

    save/load ban list, initialize scheduler

--- de/endlesscraft/TimeBan/TimeBan.java
+++ de/endlesscraft/TimeBan/TimeBan.java
@@ -1,5 +1,6 @@
 package de.endlesscraft.TimeBan;
 
+import java.io.File;
 import java.util.logging.Logger;
 
 import org.bukkit.plugin.java.JavaPlugin;
@@ -16,14 +17,24 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class TimeBan extends JavaPlugin
 {
 	Logger log = Logger.getLogger("Minecraft");
+	public BanSet banSet = new BanSet();
 	
+	/**
+	 * Load a ban list, initialize Listener of TimeBan event and a synchronous scheduler. 
+	 */
 	public void onEnable() {
-		this.getServer().getPluginManager().registerEvents(new BanListener(this), this);
-		log.info("Hey, this is TimeBan 0.1");
-	}
+		this.banSet.load(new File("plugins/TimeBan/banlist.dat"), this);
 		
-	public void onDisable() {
-		log.info("Bye!");
+		this.getServer().getPluginManager().registerEvents(new BanListener(this), this);
+		this.getServer().getScheduler().scheduleSyncRepeatingTask(this, new TimeBanRunnable(this), 60L, 200L); // all 10 secs
+				
 	}
+	
+	/**
+	 * Save a ban list.
+	 */
+	public void onDisable() {
+		this.banSet.save(new File("plugins/TimeBan/banlist.dat"), this);
+	}		
 
 }
