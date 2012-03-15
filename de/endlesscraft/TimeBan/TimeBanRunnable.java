3b6a98df5ab9a36f698b6f709f7df693c29a5aee
fzuellich <fzuellich@gmail.com>
2012-03-15 20:45:03 +0100

    removed crazy things to keep ban list synched

--- de/endlesscraft/TimeBan/TimeBanRunnable.java
+++ de/endlesscraft/TimeBan/TimeBanRunnable.java
@@ -1,33 +1,17 @@
 package de.endlesscraft.TimeBan;
 
-import java.util.Set;
-
 /**
- * Check x seconds if there is a upcoming unban.
+ * Check if there is a player to unban.
  * @author Bambusstock
  *
  */
 public class TimeBanRunnable implements Runnable
 {
-	private Set<Ban> set;
-	
-	/**
-	 * Set a set of bans to watch for unban.
-	 * @param set
-	 */
-	public void setSet(Set<Ban> set) {
-		this.set = set;
-	}
-	
-	/**
-	 * Return the set of bans to watch for unban.
-	 * @return Set
-	 */
-	public Set<Ban> getSet() {
-		return this.set;
+	private TimeBan plugin;
+	public TimeBanRunnable(TimeBan instance) {
+		this.plugin = instance;
 	}
 	
 	public void run() {
-		// check for upcoming unbans...
 	}
 }
