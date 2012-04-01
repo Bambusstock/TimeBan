f1d4bcb8fc45d3cca2a79263ac9265db72c952e4
fzuellich <fzuellich@gmail.com>
2012-04-01 18:09:38 +0200

    go through ban list and unban players (untested)

--- me/Bambusstock/TimeBan/TimeBanRunnable.java
+++ me/Bambusstock/TimeBan/TimeBanRunnable.java
@@ -1,5 +1,10 @@
 package me.Bambusstock.TimeBan;
 
+import java.util.Calendar;
+import java.util.logging.Logger;
+
+import me.Bambusstock.TimeBan.event.TimeUnbanEvent;
+
 /**
  * Check if there is a player to unban.
  * @author Bambusstock
@@ -7,12 +12,21 @@ package me.Bambusstock.TimeBan;
  */
 public class TimeBanRunnable implements Runnable
 {
+	Logger log = Logger.getLogger("Minecraft");
 	private TimeBan plugin;
+	
 	public TimeBanRunnable(TimeBan instance) {
 		this.plugin = instance;
 	}
 	
 	public void run() {
-		this.plugin.getServer();
+		if(this.plugin.banSet.isEmpty()) return;
+		log.info("[TimeBan Scheduler] Go to check if there some guys to unban...");
+		
+		// go through list until unban date is after now
+		while(this.plugin.banSet.last().until.after(Calendar.getInstance())) {
+			TimeUnbanEvent event = new TimeUnbanEvent(this.plugin.banSet.last());
+			this.plugin.getServer().getPluginManager().callEvent(event);
+		}
 	}
 }
