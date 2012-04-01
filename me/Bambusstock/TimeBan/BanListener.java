6d70cb9c181f2b58cc234c9a4ee26d7479d4d811
fzuellich <fzuellich@gmail.com>
2012-04-01 18:05:26 +0200

    - players get kicked with custom messge - unban event add sender to log message if available

--- me/Bambusstock/TimeBan/BanListener.java
+++ me/Bambusstock/TimeBan/BanListener.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 import me.Bambusstock.TimeBan.event.TimeBanEvent;
 import me.Bambusstock.TimeBan.event.TimeUnbanEvent;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -23,15 +24,23 @@ public class BanListener implements Listener
 	
 	@EventHandler
 	public void onTimeUnbanEvent(TimeUnbanEvent event) {
-		event.getBan().getPlayer().setBanned(false);
-		this.plugin.banSet.remove(event.getBan());
-		log.info("Unbaned and removed `" + event.getBan().getPlayer().getName() + "` from ban list.");	
+		Ban ban = event.getBan();
+		ban.getPlayer().setBanned(false);
+		this.plugin.banSet.remove(ban);
+		if(event.isSenderPlayer()) {
+			log.info("Unbaned and removed `" + event.getBan().getPlayer().getName() + "` from ban list by " + event.getSender());
+		} 
+		else {
+			log.info("Unbaned and removed `" + event.getBan().getPlayer().getName() + " ` from ban list.");
+		}
 	}
 	
 	@EventHandler
 	public void onTimeBanEvent(TimeBanEvent event) {
-		event.getBan().getPlayer().setBanned(true);
-		this.plugin.banSet.add(event.getBan());
-		log.info("Banned `"+ event.getBan().getPlayer().getName() + "` until " + event.getBan().until.getTime() + " by " + event.getSender());
+		Ban ban = event.getBan();
+		ban.getPlayer().setBanned(true);
+		this.plugin.banSet.add(ban);
+		((Player)ban.getPlayer()).kickPlayer(ban.getReason());
+		log.info("Banned `"+ ban.getPlayer().getName() + "` until " + ban.until.getTime() + " by " + event.getSender());
 	}
 }
