ca5edde928e2660c55b16ea1d84f116ff473442c
fzuellich <fzuellich@gmail.com>
2012-04-02 14:00:22 +0200

    minor improvements on code quality

--- me/Bambusstock/TimeBan/BanListener.java
+++ me/Bambusstock/TimeBan/BanListener.java
@@ -4,7 +4,9 @@ import java.util.logging.Logger;
 
 import me.Bambusstock.TimeBan.event.TimeBanEvent;
 import me.Bambusstock.TimeBan.event.TimeUnbanEvent;
+import me.Bambusstock.TimeBan.util.Ban;
 
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -24,23 +26,34 @@ public class BanListener implements Listener
 	
 	@EventHandler
 	public void onTimeUnbanEvent(TimeUnbanEvent event) {
-		Ban ban = event.getBan();
-		ban.getPlayer().setBanned(false);
-		this.plugin.banSet.remove(ban);
+		OfflinePlayer player = event.getBan().getPlayer();
+		
+		player.setBanned(false);
+		this.plugin.banSet.remove(event.getBan());
+		
 		if(event.isSenderPlayer()) {
-			log.info("Unbaned and removed `" + event.getBan().getPlayer().getName() + "` from ban list by " + event.getSender());
+			log.info("Unbaned and removed `" + player.getName() + "` from ban list by " + event.getSender());
 		} 
 		else {
-			log.info("Unbaned and removed `" + event.getBan().getPlayer().getName() + " ` from ban list.");
+			log.info("Unbaned and removed `" + player.getName() + " ` from ban list.");
 		}
 	}
 	
 	@EventHandler
 	public void onTimeBanEvent(TimeBanEvent event) {
 		Ban ban = event.getBan();
-		ban.getPlayer().setBanned(true);
+		OfflinePlayer player = ban.getPlayer();
+		
+		player.setBanned(true);
+		((Player) player).kickPlayer(ban.getReason());
 		this.plugin.banSet.add(ban);
-		((Player)ban.getPlayer()).kickPlayer(ban.getReason());
-		log.info("Banned `"+ ban.getPlayer().getName() + "` until " + ban.until.getTime() + " by " + event.getSender());
+		
+		if(event.isSenderPlayer()) {
+			log.info("Banned `"+ player.getName() + "` until " + ban.until.getTime() + " by " + event.getSender());
+			event.getSender().sendMessage("Banned `"+ player.getName() + "` until " + ban.until.getTime());
+		}
+		else {
+			log.info("Banned `"+ player.getName() + "` until " + ban.until.getTime());
+		}
 	}
 }
