0433ae2bf863d68ecb6d2c17ebdb25cdf752e8a2
fzuellich <fzuellich@gmail.com>
2012-04-03 19:34:22 +0200

    removed comment

--- me/Bambusstock/TimeBan/TimeBanRunnable.java
+++ me/Bambusstock/TimeBan/TimeBanRunnable.java
@@ -24,7 +24,6 @@ public class TimeBanRunnable implements Runnable
 		log.info("[TimeBan Scheduler] Go to check if there some guys to unban...");
 		
 		// go through list until unban date is in future
-		// should execute a command?
 		while(this.plugin.banSet.last().until.after(Calendar.getInstance())) {
 			TimeUnbanEvent event = new TimeUnbanEvent(this.plugin.banSet.last());
 			this.plugin.getServer().getPluginManager().callEvent(event);
