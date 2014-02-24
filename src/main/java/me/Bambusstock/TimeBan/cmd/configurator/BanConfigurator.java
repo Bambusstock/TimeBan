ed4d6d48d2ea4c7a574040cbe531660a08659df7
fzuellich <fzuellich@gmail.com>
2014-02-24 22:05:38 +0100

    + use ban command with human readable date * minor refactoring

--- src/main/java/me/Bambusstock/TimeBan/cmd/configurator/BanConfigurator.java
+++ src/main/java/me/Bambusstock/TimeBan/cmd/configurator/BanConfigurator.java
@@ -1,5 +1,7 @@
 package me.Bambusstock.TimeBan.cmd.configurator;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
 import java.util.logging.Level;
@@ -23,14 +25,22 @@ public class BanConfigurator extends AbstractConfigurator {
     // default ban duration
     private final String stdBanDuration;
 
+    // parse a date or use simple format
+    private final boolean banUntilDate;
+
+    // date format provided by config
+    private final SimpleDateFormat banDateParser;
+
     // global configuration
-    private ConfigurationSection globalConfig;
+    private final ConfigurationSection config;
 
-    public BanConfigurator(ConfigurationSection globalConfig) {
-        stdBanDuration = globalConfig.getString("defaultBanDuration", "1h");
-        stdBanReason = globalConfig.getString("defaultBanReason", "No reason given.");
+    public BanConfigurator(ConfigurationSection config) {
+        stdBanDuration = config.getString("defaultBanDuration", "1h");
+        stdBanReason = config.getString("defaultBanReason", "No reason given.");
+        banUntilDate = config.getBoolean("banUntilDate", false);
+        banDateParser = new SimpleDateFormat(config.getString("dateFormat", "dd.MM.yyyy-HH:mm"));
 
-        this.globalConfig = globalConfig;
+        this.config = config;
     }
 
     @Override
@@ -54,11 +64,10 @@ public class BanConfigurator extends AbstractConfigurator {
                 return;
             } else {
                 reason = stdBanReason;
-                until = UntilStringParser.parse(args[1]);
+                until = getCalendarFromArg(args[1]);
             }
         } else if (args.length == 3) {
             // every parameter given
-
             reason = CommandLineParser.getPrettyString(args[2]);
             until = UntilStringParser.parse(args[1]);
         } else if (args.length > 3) {
@@ -79,7 +88,7 @@ public class BanConfigurator extends AbstractConfigurator {
      *
      */
     protected void setAttributesByShortcut(String shortcut, TimeBanBanCommand command) {
-        ConfigurationSection shortcuts = globalConfig.getConfigurationSection("shortcuts");
+        ConfigurationSection shortcuts = config.getConfigurationSection("shortcuts");
 
         // configure reason
         String reason = shortcuts.getConfigurationSection(shortcut).getString("reason");
@@ -96,6 +105,27 @@ public class BanConfigurator extends AbstractConfigurator {
         } else {
             command.setUntil(UntilStringParser.parse(durationRaw));
         }
+    }
+
+    /**
+     * Get a calendar object from the given string.
+     * 
+     * @param dateString date as string according to the format given in the config.
+     */
+    protected Calendar getCalendarFromArg(String dateString) {
+        Calendar result = null;
+        if (banUntilDate) {
+            try {
+                result = Calendar.getInstance();
+                result.setTime(banDateParser.parse(dateString));
+            } catch (ParseException e) {
+                log.log(Level.SEVERE, "Error parsing given date({0})!", dateString);
+            }
+        } else {
+            result = UntilStringParser.parse(dateString);
+        }
+        
+        return result;
     }
 
 }
